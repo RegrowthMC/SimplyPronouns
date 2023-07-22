e380d9ccf3ba6fdd7a7fb5a6d529f03d7b1e3780
davidbryce <cooldcb@gmail.com>
2023-07-22 08:41:41 +0100

    Initial Commit

---
+++ src/main/java/me/dave/pronounsplus/PronounsPlus.java
@@ -0,0 +1,17 @@
+package me.dave.pronounsplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PronounsPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
